8f7df0fcb15b510d9be1ffe7a822ac8de2d4b6bd
dkmqflx <dkmqflx@gmail.com>
2023-10-25 21:36:08 +0900

    ch03. 요소를 찾는 쿼리 getBy~ Queries

--- src/MyPage.js
+++ src/MyPage.js
@@ -1,14 +1,19 @@
 export default function MyPage({ user }) {
   return (
     <div>
-      {user && user.name ? (
-        <h1>{user.name}님 환영합니다</h1>
-      ) : (
-        <h1>
-          로그인을 해주세요
-          <button>로그인</button>
-        </h1>
-      )}
+      <h1>h1</h1>
+      <h2>h2</h2>
+      <div>
+        <label htmlFor="username">이름</label>
+        <input type="text" id="username" value="Tom" readOnly></input>
+      </div>
+
+      <div data-testid="my-div" />
+
+      <div>
+        <label htmlFor="profile">자기소개</label>
+        <textarea id="profile"></textarea>
+      </div>
     </div>
   );
 }
--- src/MyPage.test.js
+++ src/MyPage.test.js
@@ -2,36 +2,54 @@ import { render, screen } from "@testing-library/react";
 
 import MyPage from "./MyPage";
 
-test("유저가 없으면 로그인 문구와 버튼을 보여준다", () => {
+test("제목이 있다", () => {
   render(<MyPage />);
 
-  const textEl = screen.getByText(/로그인을 해주세요/);
-  const btnEl = screen.getByRole("button");
+  // const titleEl = screen.getByRole("heading");
+  // getByRole은 aria-role을 기준으로 찾는다
+  // h1 ~ h6: heading
+  // button: button
+  // a: link
+  // checkbox: checkbox
+  // radio: radio
+  // select: combobox
+  // 만약 heading 태그가 두개라면 테스트 실패하게 된다
 
-  // text와 button이 document에 있다
-  expect(textEl).toBeInTheDocument();
-  expect(btnEl).toBeInTheDocument();
+  // 이러한 문제를 해결하기 위해서는 아래처럼 level을 지정하면,
+  // h1 하나에 대해서만 찾게 되므로 테스트를 통과하게 된다
+  const titleEl = screen.getByRole("heading", {
+    level: 1,
+  });
 
-  // button은 로그인이라는 텍스트를 가지고 있다.
-  expect(btnEl).toHaveTextContent("로그인");
-});
+  expect(titleEl).toBeInTheDocument();
 
-// 정상적으로 name을 가진 유저 객체가 전달되는 경우
-test("유저가 있으면 환영 문구를 보여준다", () => {
-  render(<MyPage user={{ name: "Kim" }} />);
+  // option 지정해주지 않으면 에러가 발생하는데
+  // 그 이유는 input과 textare 모두 role이 tetbox이기 때문이다.
+  // 그렇기 때문에 아래처럼 option을 지정해주면 된다.
+  // label의 name이 자기소개인 텍스트를 찾는 것이다
+  // id와 htmlFor가 연결되어 있기 때문에 가능한 것
+  const inputEl = screen.getByRole("textbox", {
+    name: "자기소개",
+  });
 
-  const textEl = screen.getByText(/Kim님 환영합니다/);
-  expect(textEl).toBeInTheDocument();
-});
+  // option을 전달하는 것이 번거롭다면 아래처럼 getByLabelText를 사용할 수도 있다.
+  // const inputEl = screen.getByLabelText("자기소개");
 
-// name을 가진 유저 객체가 전달되지 않는 테스트가 실패하는 케이스
-test("유저가 name이 없으면 로그인 문구와 버튼을 보여준다", () => {
-  render(<MyPage user="Park" />);
+  // 그리고 만약 input과 textare의 label이 모두 자기소개로 동일한경우
+  // 아래처럼 selector라는 옵션을 넣어주면 해결할 수 있다
+  // const inputEl = screen.getByLabelText("자기소개", {
+  //   selector: "textarea",
+  // });
+  expect(inputEl).toBeInTheDocument();
 
-  const textEl = screen.getByText(/로그인을 해주세요/);
-  const btnEl = screen.getByRole("button");
+  // 만약 value가 있다면 getByDisplayValue를 사용할 수 도 있다
+  const inputEl2 = screen.getByDisplayValue("Tom");
+  expect(inputEl2).toBeInTheDocument();
 
-  expect(textEl).toBeInTheDocument();
-  expect(btnEl).toBeInTheDocument();
-  expect(btnEl).toHaveTextContent("로그인");
+  // 이 외에도 getByPlaceholderText, getByTitle 등이 있다.
+
+  // 지금까지 다루었던 쿼리로 찾을 수 없을 때 getByTestId를 사용한다
+  // 테스트를 위해 프로젝트와 관련 없는 dataset-id를 추가하기 때문에 최후의 수단이다
+  const myDiv = screen.getByTestId("my-div");
+  expect(myDiv).toBeInTheDocument();
 });
